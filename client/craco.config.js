038abebe647b37dc0d9a15d9551045ca353ecc8a
Hadi <61531010+hadisuria@users.noreply.github.com>
2021-09-28 08:48:00 +0700

    update craco set intellisence for vscode
    Update intellisense for vscode
    Update path configuration

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,9 +1,18 @@
 const path = require("path");
+const alias = require(`./src/config/aliases`);
+
+const SRC = `./src`;
+const aliases = alias(SRC);
+
+const resolvedAliases = Object.fromEntries(
+	Object.entries(aliases).map(([key, value]) => [
+		key,
+		path.resolve(__dirname, value),
+	])
+);
 
 module.exports = {
 	webpack: {
-		alias: {
-			"@components": path.resolve(__dirname, "src/components/"),
-		},
+		alias: resolvedAliases,
 	},
 };
---
+++ client/src/config/aliases.js
@@ -0,0 +1,7 @@
+const aliases = (prefix = `src`) => ({
+	"@components": `${prefix}/components`,
+	"@config": `${prefix}/config`,
+	"@pages": `${prefix}/pages`,
+});
+
+module.exports = aliases;
