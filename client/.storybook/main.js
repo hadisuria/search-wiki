3457066d6052ad99eec1bc331f43bfb6eac2fa60
Hadi <61531010+hadisuria@users.noreply.github.com>
2021-09-29 10:03:39 +0700

    Update storybook configuration
    - Fix issue storybook import using alias to work with craco. Fixed by custom webpack configuration for storybook

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,11 +1,23 @@
+const path = require("path");
+const cracoConfig = require("../craco.config");
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+	stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/preset-create-react-app",
+	],
+	webpackFinal: async (config, { configType }) => {
+		return {
+			...config,
+			resolve: {
+				...config.resolve,
+				alias: {
+					...config.resolve.alias,
+					...cracoConfig.webpack.alias,
+				},
+			},
+		};
+	},
+};
