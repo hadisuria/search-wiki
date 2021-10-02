fcc9a0c3a78c641b25b8d3b013c469c81fa467e9
Hadi <61531010+hadisuria@users.noreply.github.com>
2021-10-02 15:33:21 +0700

    Update test autocomplete

--- client/src/components/Autocomplete/index.test.js
+++ client/src/components/Autocomplete/index.test.js
@@ -18,17 +18,64 @@ describe("Autocomplete component", () => {
 		});
 	});
 
-	// describe("with props", () => {
-	// 	beforeEach(() => {
-	// 		props = {
-	// 			articles: [],
-	// 			searchValue: "",
-	// 			onSearchChange: jest.fn(),
-	// 		};
-	// 		sut = render(props);
-	// 	});
-	// 	it("should match snapshot", () => {
-	// 		expect(sut).toMatchSnapshot();
-	// 	});
-	// });
+	describe("when articles are passed", () => {
+		beforeEach(() => {
+			props = {
+				articles: [1],
+				searchValue: "Search value",
+				onSearchChange: jest.fn(),
+			};
+			sut = render(props);
+		});
+		it("should match snapshot", () => {
+			const props = sut.props();
+
+			expect(props.renderMenu()).toMatchSnapshot();
+		});
+	});
+	describe("when articles are NOT passed", () => {
+		beforeEach(() => {
+			props = {
+				articles: null,
+				searchValue: "Search value",
+				onSearchChange: jest.fn(),
+			};
+			sut = render(props);
+		});
+		it("should match snapshot", () => {
+			const props = sut.props();
+
+			expect(props.renderMenu()).toMatchSnapshot();
+		});
+	});
+
+	describe("when renderItems prop is called", () => {
+		beforeEach(() => {
+			sut = render(props);
+		});
+
+		describe("When item is highlighted", () => {
+			it("should match snapshot", () => {
+				const { renderItem } = sut.props();
+				const item = {
+					id: "https://en.wikipedia.org/wiki/Elon_Musk",
+					label: "Elon Musk",
+				};
+				const highlighted = true;
+				expect(renderItem(item, highlighted)).toMatchSnapshot();
+			});
+		});
+
+		describe("When item is NOT highlighted", () => {
+			it("should match snapshot", () => {
+				const { renderItem } = sut.props();
+				const item = {
+					id: "https://en.wikipedia.org/wiki/Elon_Musk",
+					label: "Elon Musk",
+				};
+				const highlighted = false;
+				expect(renderItem(item, highlighted)).toMatchSnapshot();
+			});
+		});
+	});
 });
