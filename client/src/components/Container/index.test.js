a25f63a9890cc8db35db6ed36f0634f885f42b86
hadisuria <hadisuria789@gmail.com>
2021-10-04 18:16:20 +0700

    update container test

--- client/src/components/Container/index.test.js
+++ client/src/components/Container/index.test.js
@@ -1,7 +1,18 @@
 import React from "react";
 import { mount } from "enzyme";
 
-import Container from ".";
+import Container from "./index.js";
+
+jest.mock("@hooks", () => ({
+	useDebounce: jest.fn(() => ({})),
+	useSearch: () => ({ articles: [] }),
+	useSearchForm: () => {
+		return {
+			searchValue: "testValue",
+			onSearchChange: () => {},
+		};
+	},
+}));
 
 const render = (props, children) =>
 	mount(<Container {...props}>{children}</Container>);
@@ -26,6 +37,20 @@ describe("Container component", () => {
 
 		it("should match snapshot", () => {
 			expect(sut).toMatchSnapshot();
+		});
+	});
+
+	describe("when passed custom props", () => {
+		beforeEach(() => {
+			sut = render(props, children);
+		});
+
+		it("should return search value in children component", () => {
+			const { searchValue, onSearchChange, articles } = sut
+				.find("Children")
+				.props();
+
+			expect(searchValue).toBe("testValue");
 		});
 	});
 });
